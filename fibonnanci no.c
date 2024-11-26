097d94dea0e660f56c7b2f461bb03af96dea33aa
Swayam123-alt <swayamsurpur@gmail.com>
2024-11-26 03:10:47 -0800

    fibonnanci no.c

---
+++ fibonnanci no.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int n,t1=0,t2=1,nextterm;
+  printf("enter the number of terms:");
+  scanf("%d",&n);
+  printf("fibonancci series:%d,%d",t1,t2);
+  for(int i=3;i<=n;i++)
+    {
+    nextterm=t1+t2;
+    printf("%d",nextterm);
+    t1=t2;
+    t2=nextterm;
+  }
+  return 0;
+    
+}
